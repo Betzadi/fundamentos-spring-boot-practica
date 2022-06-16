c0bc8cdf083207816d5d5df0fe3de5bd0a92f9d8
Betzadi Vega <betzadi.vega@gmail.com>
2022-06-16 18:32:58 -0500

    Agregadas nuevas configuraciones

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,32 +2,71 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.entity.User;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentos.springboot.fundamentos.repository.UserRepository;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
-	private ComponentDependency componentDependency;
-	private MyBean myBean;
-	private MyBeanWithDependency myBeanWithDependency;
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,MyBean myBean,MyBeanWithDependency myBeanWithDependency){
-		this.componentDependency=componentDependency;
-		this.myBean=myBean;
-		this.myBeanWithDependency=myBeanWithDependency;
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundamentosApplication.class, args);
-	}
+    private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+    private ComponentDependency componentDependency;
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+    private MyBeanWithProperties myBeanWithProperties;
+
+    private UserPojo userPojo;
+
+    private UserRepository userRepository;
+
+    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency,UserRepository userRepository, UserPojo userPojo, MyBeanWithProperties myBeanWithProperties) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+        this.myBeanWithProperties = myBeanWithProperties;
+        this.userPojo = userPojo;
+        this.userRepository = userRepository;
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosApplication.class, args);
+    }
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		componentDependency.saludar();
-		myBean.print();
-		myBeanWithDependency.printWithDependency();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        ejemplosAnteriores();
+        saveUserInDataBase();
+    }
+
+    private void saveUserInDataBase() {
+        User user1 = new User("jhon", "jhon@gmail.com", LocalDate.of(2021, 02, 12));
+        User user2 = new User("jimmy", "jimmy@gmail.com", LocalDate.of(2021, 02, 12));
+        User user3 = new User("sam", "sam@gmail.com", LocalDate.of(2021, 02, 12));
+        List<User> list = Arrays.asList(user1, user2, user3);
+        list.stream().forEach(userRepository::save);
+    }
+
+    private void ejemplosAnteriores() {
+        componentDependency.saludar();
+        myBean.print();
+        myBeanWithDependency.printWithDependency();
+        System.out.println(myBeanWithProperties.function());
+        System.out.print(userPojo.getEmail());
+        LOGGER.error("Esto es un error");
+    }
 }
